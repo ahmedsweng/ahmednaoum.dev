f37694d37235e027b2d20f2a54f831e6b283e9c1
AhmedNaoumvdev <raoufnaoum15@gmail.com>
2023-10-13 12:23:34 +0100

    fix bio

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Hi, my name is Ahmed, and I'm a full-stack developer and freelancer.
+          Hi, my name is Ahmed, I'm a full-stack developer and freelancer.
           <br />
           and building open-source projects at{" "}
           <Link
