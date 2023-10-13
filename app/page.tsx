6daa32c07672b75504d12bd7d8141e3f3ef264c0
AhmedNaoumvdev <raoufnaoum15@gmail.com>
2023-10-13 11:34:58 +0100

    fixing font responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -40,13 +40,19 @@ export default function Home() {
       <h1 className="z-10 text-4xl text-transparent duration-500 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         Ahmed Naoum
       </h1>
-      <div className="flex flex-row gap-5">
-        <h2 className="text-4xl font-semibold text-[#667eea] ">React, </h2>
-        <h2 className="text-4xl font-semibold text-[#667eea]">
+      <div className="flex flex-row gap-2">
+        <h2 className="text-1xl sm:text-2xl md:text-4xl font-semibold text-[#667eea] ">
+          React,{" "}
+        </h2>
+        <h2 className="text-1xl sm:text-2xl md:text-4xl font-semibold text-[#667eea]">
           React Native,{" "}
         </h2>
-        <h2 className="text-4xl font-semibold text-[#667eea]">Node, </h2>
-        <h2 className="text-4xl font-semibold text-[#667eea]">TypeScript</h2>
+        <h2 className="text-1xl sm:text-2xl md:text-4xl font-semibold text-[#667eea]">
+          Node,{" "}
+        </h2>
+        <h2 className="text-1xl sm:text-2xl md:text-4xl font-semibold text-[#667eea]">
+          TypeScript
+        </h2>
       </div>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
